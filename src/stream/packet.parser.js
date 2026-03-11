60d689527e4a6721d57f9a6d5ec1678ad229d20d
kjaxi <khandakerjim@gmail.com>
2026-03-11 16:22:57 +0600

    Quick Fix: parser regex updated

--- src/stream/packet.parser.js
+++ src/stream/packet.parser.js
@@ -1,22 +1,22 @@
 function parsePacketLine(line) {
+// Example line:
+// 1773224441.786334 eth0  Out IP 172.21.142.134.58380 > 185.125.190.57.123: UDP, length 48
 
-    console.log("RAW LINE:", line);  // DEBUG
-// Example tcpdump line:
-// 1710000.123456 IP 192.168.1.10.54822 > 8.8.8.8.443: Flags [S], seq 12345, length 0
+const regex = /^(\d+.\d+)\s+\S+\s+(In|Out|M)?\s*IP\s+([0-9.]+).(\d+)\s+>\s+([0-9.]+).(\d+):\s*(TCP|UDP)?,?\s*length\s+(\d+)/;
 
-const match = line.match(/^(\d+.\d+)\s+IP\s+([0-9.]+).(\d+)\s+>\s+([0-9.]+).(\d+).*length\s+(\d+)/);
-
+const match = line.match(regex);
 if (!match) return null;
 
-const [, timestamp, srcIP, srcPort, destIP, destPort, length] = match;
+const [, timestamp, direction, srcIP, srcPort, destIP, destPort, protocol, length] = match;
 
 return {
 timestamp: parseFloat(timestamp),
+direction: direction || "Unknown",
 srcIP,
 srcPort: Number(srcPort),
 destIP,
 destPort: Number(destPort),
-protocol: line.includes("UDP") ? "UDP" : "TCP",
+protocol: protocol || "TCP",
 length: Number(length)
 };
 }
